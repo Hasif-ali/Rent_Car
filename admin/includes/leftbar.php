4f750486dd5e02591eb3ba376a9167da46d1d21f
Hasif-ali <153004809+Hasif-ali@users.noreply.github.com>
2024-09-08 17:41:30 +0530

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,40 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+			
+			
+				<li><a href="dashboard.php"><i class="fa fa-home"></i> 
+				Home
+				</a></li>
+			
+
+
+
+				<li><a href="manage-bookings.php"><i class="fa fa-check-circle"></i> Manage Renting</a></li>
+
+	<li><a href="#"><i class="fa fa-check-circle"></i> Vehicles</a>
+					<ul>
+						<li><a href="post-avehical.php">Post a Vehicle</a></li>
+						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+					</ul>
+				</li>	 
+		 
+	<li><a href="#"><i class="fa fa-check-circle"></i> Brands</a>
+<ul>
+<li><a href="create-brand.php">Create Brand</a></li>
+<li><a href="manage-brands.php">Manage Brands</a></li>
+</ul>
+</li>	 
+		 
+				<li><a href="manage-conactusquery.php"><i class="fa fa-check-circle""></i> Manage   Query</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-check-circle"></i>Customers
+				</a></li>
+			 
+<li><a href="change-password.php"><i class="fa fa-check-circle"></i>Password
+				</a></li>
+
+<li><a href="logout.php"><i class="fa fa-check-circle"></i>Logout
+				</a></li>
+
+			</ul>
+		</nav>
